11d14503caed80ee005abc2f0bfd268e8d72a882
jjavierf64 <jjavierf64@estudiantec.cr>
2023-12-04 15:49:26 -0600

    Arreglo de Datos Fluke

--- Software Comparador/Server/FuncionesServer.py
+++ Software Comparador/Server/FuncionesServer.py
@@ -129,10 +129,7 @@ serFluke=serial.Serial("/dev/ttyUSBK", baudrate=9600, bytesize=serial.EIGHTBITS,
 
 def DatosFluke():
     
-    serFluke.write(b"READ?1\r\n") #Envío de instrucción para capturar dato de temperatura 1
-    serFluke.write(b"READ?2\r\n") #Envío de instrucción para capturar dato de temperatura 2
-    serFluke.write(b"READ?3\r\n") #Envío de instrucción para capturar dato de temperatura 3
-    serFluke.write(b"READ?4\r\n") #Envío de instrucción para capturar dato de temperatura 4
+    serFluke.flushInput()
 
     detenerse=0 #Constante para while que captura dato
     
@@ -151,18 +148,66 @@ def DatosFluke():
                 break
             sleep(0.02)
         return data
-    while detenerse == 0:
+
+
+    while detenerse <= 3:
         data=recv(serFluke) #Llamada de la función
 
         if data != b"": #Comparación de datos recibidos, vacío hasta que se de la medición
-            todas=data.split()#Separar los 4 datos en una lista
-            print(todas)
-            MedicionTemp1=float(todas[0]) #Guardando temperatura 1 en lista
-            MedicionTemp2=float(todas[1]) #Guardando temperatura 2 en lista
-            MedicionTemp3=float(todas[2]) #Guardando temperatura 3 en lista
-            MedicionTemp4=float(todas[3]) #Guardando temperatura 4 en lista
-            detenerse = 1  #Condición para salir del while
+            medicion=data.split()#Separar los 4 datos en una lista
+            
+            if int(medicion[0]) == 1 and MedicionTemp1==0:
+                MedicionTemp1=float(medicion[1]) #Guardando temperatura 1 en lista
+                detenerse += 1  #Condición para salir del while
+            elif int(medicion[0]) == 1 and MedicionTemp2==0:
+                MedicionTemp2=float(medicion[1]) #Guardando temperatura 1 en lista
+                detenerse += 1  #Condición para salir del while
+            elif int(medicion[0]) == 1 and MedicionTemp3==0:
+                MedicionTemp3=float(medicion[1]) #Guardando temperatura 1 en lista
+                detenerse += 1  #Condición para salir del while
+            elif int(medicion[0]) == 1 and MedicionTemp4==0:
+                MedicionTemp4=float(medicion[1]) #Guardando temperatura 1 en lista
+                detenerse += 1  #Condición para salir del while
+
     return MedicionTemp1, MedicionTemp2, MedicionTemp3, MedicionTemp4
+
+
+# def DatosFluke():
+    
+#     serFluke.write(b"READ?1\r\n") #Envío de instrucción para capturar dato de temperatura 1
+#     serFluke.write(b"READ?2\r\n") #Envío de instrucción para capturar dato de temperatura 2
+#     serFluke.write(b"READ?3\r\n") #Envío de instrucción para capturar dato de temperatura 3
+#     serFluke.write(b"READ?4\r\n") #Envío de instrucción para capturar dato de temperatura 4
+
+#     detenerse=0 #Constante para while que captura dato
+    
+#     MedicionTemp1=0 #Creación de variable para almacenar mediciones de temperatura 1
+#     MedicionTemp2=0 #Creación de variable para almacenar mediciones de temperatura 2
+#     MedicionTemp3=0 #Creación de variable para almacenar mediciones de temperatura 3
+#     MedicionTemp4=0 #Creación de variable para almacenar mediciones de temperatura 4
+
+
+#     def recv(serial): #Definición de una función para recibir datos
+#         while True:
+#             data=serial.read(32) #Lectura de 32 bytes
+#             if data == "":
+#                 continue
+#             else:
+#                 break
+#             sleep(0.02)
+#         return data
+#     while detenerse == 0:
+#         data=recv(serFluke) #Llamada de la función
+
+#         if data != b"": #Comparación de datos recibidos, vacío hasta que se de la medición
+#             todas=data.split()#Separar los 4 datos en una lista
+#             print(todas)
+#             MedicionTemp1=float(todas[0]) #Guardando temperatura 1 en lista
+#             MedicionTemp2=float(todas[1]) #Guardando temperatura 2 en lista
+#             MedicionTemp3=float(todas[2]) #Guardando temperatura 3 en lista
+#             MedicionTemp4=float(todas[3]) #Guardando temperatura 4 en lista
+#             detenerse = 1  #Condición para salir del while
+#     return MedicionTemp1, MedicionTemp2, MedicionTemp3, MedicionTemp4
 
 
 
