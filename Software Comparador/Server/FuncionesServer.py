ffadbe1d459cbae5280eb9f838f5b4f950adc33f
jjavierf64 <jjavierf64@estudiantec.cr>
2023-12-05 10:09:25 -0600

    fix Mediciones Fluke

--- Software Comparador/Server/FuncionesServer.py
+++ Software Comparador/Server/FuncionesServer.py
@@ -158,15 +158,19 @@ def DatosFluke():
             
             if int(medicion[0]) == 1 and MedicionTemp1==0:
                 MedicionTemp1=float(medicion[1]) #Guardando temperatura 1 en lista
+                print(MedicionTemp1," del 1")
                 detenerse += 1  #Condición para salir del while
             elif int(medicion[0]) == 1 and MedicionTemp2==0:
                 MedicionTemp2=float(medicion[1]) #Guardando temperatura 1 en lista
+                print(MedicionTemp2," del 2")
                 detenerse += 1  #Condición para salir del while
             elif int(medicion[0]) == 1 and MedicionTemp3==0:
                 MedicionTemp3=float(medicion[1]) #Guardando temperatura 1 en lista
+                print(MedicionTemp3," del 3")
                 detenerse += 1  #Condición para salir del while
             elif int(medicion[0]) == 1 and MedicionTemp4==0:
                 MedicionTemp4=float(medicion[1]) #Guardando temperatura 1 en lista
+                print(MedicionTemp4," del 4")
                 detenerse += 1  #Condición para salir del while
 
     return MedicionTemp1, MedicionTemp2, MedicionTemp3, MedicionTemp4
